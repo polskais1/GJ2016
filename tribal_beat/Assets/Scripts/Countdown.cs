661b8c869d5fdc95c113ac2cb980cbceea76cedf
sidedishes <sidedishf@gmail.com>
2016-01-31 11:19:59 -0500

    countdown

--- tribal_beat/Assets/Scripts/Countdown.cs
+++ tribal_beat/Assets/Scripts/Countdown.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Countdown : MonoBehaviour {
 
-	// Use this for initialization
+    public int secondsToRun = 61;
+    private float endTime;
+
+    public Text counter, leftStatus, rightStatus;
+
 	void Start () {
-	
+        endTime = Time.time + secondsToRun;
+        leftStatus.text = rightStatus.text = "";
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+        counter.text = "" + secondsRemaining();
+        if (timeUp())
+        {
+            leftStatus.text = "LOSER";
+            rightStatus.text = "WINNER";
+        }
 	}
+
+    int secondsRemaining()
+    {
+        return (int)(endTime - Time.time);
+    }
+
+    bool timeUp()
+    {
+        return (endTime <= Time.time);
+    }
 }

ac3b763c4365b7582a7ea0c17c4530d4a0f1ca9c
sidedishes <sidedishf@gmail.com>
2016-01-31 03:15:34 -0500

    notes fade in and out

---
+++ tribal_beat/Assets/Scripts/FadeAway.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeAway : MonoBehaviour {
+    public float timeToFade = 0.5f;
+    private float startTime;
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	void Update () {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - (Time.time - startTime)/timeToFade);
+        if (Time.time - startTime > timeToFade)
+            Destroy(gameObject);
+	}
+}
---
+++ tribal_beat/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour
+{
+    public float timeToFade = 0.5f;
+    private float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, Mathf.Min(1, (Time.time - startTime) / timeToFade));
+        if (Time.time - startTime > timeToFade)
+            Destroy(this);
+    }
+}
